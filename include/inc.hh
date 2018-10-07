79eaa5481748272e6b00869cf986026d4ae829ab
Zohar Malamant <dotfloat@gmail.com>
2018-10-07 17:30:46 +0200

    Initial Commit

---
+++ include/inc.hh
@@ -0,0 +1,8 @@
+#ifndef __INC__40255697
+#define __INC__40255697
+
+#include <iostream>
+
+constexpr auto hello_world = "Hello, world!";
+
+#endif //__INC__40255697
---
+++ src/main.cc
@@ -0,0 +1,7 @@
+#include "inc.hh" /* CLion: "inc.hh" file not found */
+
+int main()
+{
+    /* CLion claims these aren't defined */
+    std::cout << hello_world << std::endl;
+}
